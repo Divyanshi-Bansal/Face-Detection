e099d86f698fcc918f397e4f615aa341d10e5892
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-05-03 05:10:50 +0530

    face detect by webcam

--- detect_face.py
+++ detect_face.py
@@ -3,27 +3,25 @@ import sys
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# casc_Path = sys.argv[0]
-# faceCascade = cv2.CascadeClassifier(casc_Path)
+
 # #sets the video source to the default webcam
 videoCapture = cv2.VideoCapture(0)
-# #
+
 while True:
     ret , frame = videoCapture.read()
     gray = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray ,1.1 , 4)
-#
+
     for (x,y,w,h) in faces:
         cv2.rectangle(frame , (x,y) , (x+w , y+h) , (0,255,0) , 2)
-#
+
     cv2.imshow('Video' , frame)
-#
-#     #for exiting
+
+    #for exiting--escape key--30 ascii value
     if cv2.waitKey(30) & 0xFF == ord('q'):
         break
 
-# videoCapture.release()
-# cv2.destroyAllWindows()
+
 videoCapture.release()
 
 
